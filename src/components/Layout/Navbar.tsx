f6b39ebbcfdef20499687fda36ebf10a62397d79
Naibuu <hawkwer141099@gmail.com>
2024-09-25 21:18:08 +0300

    make download modal show in nav too

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,10 +1,20 @@
+import { createSignal } from 'solid-js'
 import { A } from '@solidjs/router'
-import Button from '@/components/UI/Button'
+
 import Logo from '@/assets/logo.svg'
+import Button from '@/components/UI/Button'
+
+import DownloadModal from '@/containers/DownloadModal'
 
 export default function Navbar() {
+    const [showModal, setShowModal] = createSignal<boolean>(false)
+
     return (
         <header class="z-10 py-12 flex items-center justify-between">
+            {showModal() && (
+                <DownloadModal onclose={() => setShowModal(false)} />
+            )}
+
             <A href="/">
                 <img src={Logo} draggable={false} class="size-10" />
             </A>
@@ -18,11 +28,13 @@ export default function Navbar() {
                     FAQ
                 </A>
 
-                <A href="/download">
-                    <Button style="primary" customClass="!rounded-full text-sm">
-                        Download
-                    </Button>
-                </A>
+                <Button
+                    style="primary"
+                    customClass="!rounded-full text-sm"
+                    onclick={() => setShowModal(true)}
+                >
+                    Download
+                </Button>
             </div>
         </header>
     )
