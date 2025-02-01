b0572fdbab017d61390635f1fb6d4a4c8632a3f5
creations <creations@creations.works>
2025-02-01 11:23:59 -0500

    update stars for future, fix navbar saying plugins always

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 1000
+        port: 9000
     },
     plugins: [solidPlugin(), tsconfigPaths()],
     build: { sourcemap: true },
