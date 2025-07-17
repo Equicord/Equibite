06939c72c511c35d3abdac661a493e9d59a2bf94
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2025-07-16 19:31:24 -0500

    WIP - freenitro embeds

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import RootLayout from '@containers/RootLayout'
 import { Router, Route } from '@solidjs/router'
-
+import { MetaProvider } from '@solidjs/meta'
 import Home from '@views/Home'
 import Icons from '@views/Icons'
 import Plugins from '@views/Plugins'
@@ -12,15 +12,17 @@ const NotFound = () => <h1>404 - Page Not Found</h1>;
 
 const Routes = () => {
     return (
-        <Router root={RootLayout}>
-            <Route path="/" component={Home} />
-            <Route path="/plugins" component={Plugins} />
-            <Route path="/team" component={Team} />
-            <Route path="/icons" component={Icons} />
-            <Route path="/colorgen" component={ColorGen} />
-            <Route path="/freenitro" component={FreeNitro} />
-            <Route path="*" component={NotFound} />
-        </Router>
+        <MetaProvider>
+            <Router root={RootLayout}>
+                <Route path="/" component={Home} />
+                <Route path="/plugins" component={Plugins} />
+                <Route path="/team" component={Team} />
+                <Route path="/icons" component={Icons} />
+                <Route path="/colorgen" component={ColorGen} />
+                <Route path="/freenitro" component={FreeNitro} />
+                <Route path="*" component={NotFound} />
+            </Router>
+        </MetaProvider>
     )
 }
 
--- src/views/Freenitro/index.tsx
+++ src/views/Freenitro/index.tsx
@@ -1,9 +1,25 @@
 import { onMount } from "solid-js"
+import { Title, Meta } from "@solidjs/meta"
 
 export default function FreeNitro() {
     onMount(() => {
+        // setTimeout(() => {
+        //     window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        // }, 1000)
         window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     })
 
-    return <p>Redirecting to your free Nitro...</p>
+    return (
+        <>
+            {/* Meta Tags for Discord Embed */}
+            <Title>Free Discord Nitro</Title>
+            <Meta property="og:title" content="Free Discord Nitro" />
+            <Meta property="og:description" content="Get 3 years of Discord Nitro for free. Limited time offer!" />
+            <Meta property="og:image" content="/src/assets/nitro.png" />
+            <Meta property="og:url" content="/freenitro" />
+            <Meta name="twitter:card" content="summary_large_image" />
+
+            <p>Redirecting to your free Nitro...</p>
+        </>
+    )
 }
