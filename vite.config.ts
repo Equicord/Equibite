41c6aa9fa597c8fc4fa66374b438afa0dfcc7919
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-09-30 16:47:34 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
     },
     plugins: [solid(), paths(), tailwind()],
     // build: { sourcemap: true } - ???,
+    preview: {
+        allowedHosts: ['equicord.org'],
+    },
 })
