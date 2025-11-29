39d5c8ff13f2bfd7f842cd2c36789f89ed5b6b7b
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-11-29 15:35:31 -0500

    Use port 6000

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import paths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 3000,
+        port: 6000,
     },
     plugins: [solid(), paths(), tailwind()],
     // build: { sourcemap: true } - ???,
