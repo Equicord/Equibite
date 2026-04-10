15ce1aa858511212e68ab870931547b0ae34742e
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-04-09 21:30:20 -0400

    Update Policy.tsx

--- src/views/Cloud/Policy.tsx
+++ src/views/Cloud/Policy.tsx
@@ -13,7 +13,7 @@ export default function CloudPolicy() {
         >
             <div class="prose prose-invert max-w-none">
                 <p class="text-lg font-medium text-neutral-300 mb-8">
-                    Last Updated: October 5, 2025
+                    Last Updated: April 9, 2026
                 </p>
 
                 <section class="mb-8">
@@ -54,7 +54,7 @@ export default function CloudPolicy() {
                                 Discord User ID Hash
                             </h3>
                             <p class="text-sm font-medium text-neutral-400">
-                                We store the CRC hash of your Discord user ID to
+                                We store the SHA-256 hash of your Discord user ID to
                                 uniquely identify your data. This is a one-way
                                 hash that cannot be reversed to reveal your
                                 actual Discord ID.
@@ -66,10 +66,9 @@ export default function CloudPolicy() {
                                 Your Settings
                             </h3>
                             <p class="text-sm font-medium text-neutral-400">
-                                Your Equicord settings stored as JSON plain
-                                text. This enables our Settings Sync feature to
-                                synchronize your preferences across multiple
-                                devices.
+                                Your Equicord settings are securely stored on our servers,
+                                tied to only your generated hash and able to synchronized
+                                across devices.
                             </p>
                         </div>
                     </div>
